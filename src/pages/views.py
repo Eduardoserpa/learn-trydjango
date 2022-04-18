8b6c52a77738216577de4d0f70959940bcd2c040
Eduardo <eduardoserpa@alu.ufc.br>
2022-04-18 10:40:51 -0300

    Atualização para usar templates Django.

--- src/pages/views.py
+++ src/pages/views.py
@@ -4,10 +4,10 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home_view(request, *args, **kwargs):
-    return HttpResponse("<h1>Hello World</h1>")
-
-def contact_view(request, *args, **kwargs):
-    return HttpResponse("<h1>Contact Page</h1>")
+    return render(request, "home.html", {})
 
 def about_view(request, *args, **kwargs):
-    return HttpResponse("<h1>About this site</h1>")
+    return render(request, "about.html", {})
+
+def contact_view(request, *args, **kwargs):
+    return render(request, "contact.html", {})
